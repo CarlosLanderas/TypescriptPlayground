46e29d416f36c81b43ace9612f0c0e1e84ddad7b
Carlos Landeras <Carlos Landeras>
2016-12-04 17:09:36 +0100

    Clean dist js folder not longer needed

--- app.ts
+++
@@ -1,9 +0,0 @@
-import { UserService } from './snippets/services/userService';
-import { User, UserAddress } from './snippets/interfaces/user';
-import {Programmer} from './snippets/classes/static';
-
-
-let userService = new UserService();
-
-console.log(Programmer.main());
-console.log(userService.getAllUsers());
--- js/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var userService_1 = require('./snippets/services/userService');
-var static_1 = require('./snippets/classes/static');
-var userService = new userService_1.UserService();
-console.log(static_1.Programmer.main());
-console.log(userService.getAllUsers());
-//# sourceMappingURL=app.js.map
