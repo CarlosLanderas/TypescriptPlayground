89d38a7bf2baf9432aebb9fdce426cf4864c5d1b
Carlos Landeras <Carlos Landeras>
2016-12-06 19:27:51 +0100

    Iterators: forIn and forOf

---
+++ snippets/iterators/for.ts
@@ -0,0 +1,9 @@
+let people: string[] = ['Ernesto', 'Lucas', 'Pedro', 'Clara', 'Maria'];
+
+for(let person in people){
+    console.log(person);
+}
+
+for(let person of people){
+    console.log(person);
+}
