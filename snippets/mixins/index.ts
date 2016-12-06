9d9cef57adf088d332cd88c5b9d2e567e9bb6741
Carlos Landeras <Carlos Landeras>
2016-12-06 19:25:13 +0100

    Export applyMixins as external funciton tool

--- snippets/mixins/index.ts
+++ snippets/mixins/index.ts
@@ -1,3 +1,5 @@
+import applyMixins from "./applyMixins";
+
 class BackendProgrammer {    
     createDatabase() : boolean {
         console.log("Creating database");
@@ -17,18 +19,10 @@ class FullStackProgrammer implements FrontProgrammer, BackendProgrammer{
      createSpa: () => true;
 }
 
-
-function applyMixins(derivedCtor: any, baseCtors: any[]) {
-    baseCtors.forEach(baseCtor => {
-        Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
-            derivedCtor.prototype[name] = baseCtor.prototype[name];
-        });
-    });
-}
-
 // Apply mixings to FullStackProgrammer class
 applyMixins(FullStackProgrammer, [BackendProgrammer, FrontProgrammer]);
 
 let fullStack = new FullStackProgrammer();
 fullStack.createDatabase();
 fullStack.createSpa();
+
