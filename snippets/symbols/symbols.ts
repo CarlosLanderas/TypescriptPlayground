655969e6c43913eb1d8f7cfeae91a4025a180679
Carlos Landeras Martínez <clanderas@plainconcepts.com>
2016-12-07 17:03:43 +0100

    More symbols example, refactor needed

--- snippets/symbols/symbols.ts
+++ snippets/symbols/symbols.ts
@@ -1,4 +1,4 @@
-import arrayFirst from "./arrayPrototype";
+import arrayFirst from './arrayPrototype';
 
 let arrayNumber = [4,5,6,7,8];
 console.log(arrayNumber[arrayFirst]());
@@ -7,6 +7,25 @@ console.log(arrayNumber[arrayFirst]());
 console.log(Object.getOwnPropertyNames(Array.prototype));
 console.log(Object.getOwnPropertySymbols(Array.prototype));
 
+
+//local symbols 
+console.log(Symbol('foo') === Symbol('foo'));
+console.log(Symbol.for('foo') === Symbol.for('foo'));
+
+let person = {
+    name: "Luke",
+    surname: "Skywalker",
+    [Symbol("fullName")] : () => { console.log('Hello from symbol')}
+}
+
+console.log(person);
+
+console.log(Object.getOwnPropertyNames(person));
+
+Object.getOwnPropertySymbols(person).forEach( s => {
+       let symbol : any = person[s];
+       console.log(symbol());
+});
 
 
 
