7e0fe9f8451a551e2aa32cca64a7e511c1d1387e
Carlos Landeras <Carlos Landeras>
2016-12-15 00:23:01 +0100

    Function samples

--- snippets/06-functions/functionsExample.ts
+++ snippets/06-functions/functionsExample.ts
@@ -1,17 +1,19 @@
-function sumNumbers(... params: number[]) {
-    
-    let totalSum = 0;
-   
-    for (let i = 0; i < params.length; i++) {     
-         totalSum += params[i];
-     }
-    return totalSum;
+
+function sumNumbers(...params : number[]){
+    return params.reduce( ( total, value) => {
+        total += value;
+        return total;
+    });
 }
 
+
+const saluteFunction = (name:string) : (salute:string) => string => {
+    return (salute:string) => `${name} ${salute}!`;
+}
 
 const anonymousSum = (number1: number, number2: number) : number  =>  {
      return sumNumbers(number1, number2);
 };
 
-export {sumNumbers};
+export {sumNumbers, saluteFunction};
 export default anonymousSum;
