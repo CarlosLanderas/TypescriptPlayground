ef3230dfc38ff5ec7ae6de872e3a94e03fd6e01c
Carlos Landeras Martínez <clanderas@plainconcepts.com>
2016-12-15 12:51:46 +0100

    Changes destruc array

--- snippets/02-features/destructuring/destructuring_array.ts
+++ snippets/02-features/destructuring/destructuring_array.ts
@@ -3,11 +3,6 @@ let programmingLanguages: string[] = ['c#', 'java', 'python'];
 let first: string = programmingLanguages[0];
 let second: string = programmingLanguages[1];
 
-function getProgrammingLanguages() {
-    return programmingLanguages;
-}
-
-
 // Destructuring the array
 
 let [csharp, java, python] = programmingLanguages;
@@ -16,7 +11,14 @@ console.log(java);
 console.log(python);
 
 
-//parse array from function
+//parse array from function.
+
+
+function getProgrammingLanguages() {
+    return programmingLanguages;
+}
+
+
 
 let [lang1, lang2, lang3] = getProgrammingLanguages();
 console.log({lang1, lang2, lang3});
